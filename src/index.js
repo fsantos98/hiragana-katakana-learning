67f283237b90c3f2981c36daa3d3e549f4ba9398
fsantos98 <franciscoslb.98@hotmail.com>
2024-04-03 14:15:35 +0100

    add css

--- src/index.js
+++ src/index.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import LearnKana from './LearnKana'; // Import the LearnKana component instead of App
+import './index.css';
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
